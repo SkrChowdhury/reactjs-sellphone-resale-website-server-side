60fdf221abce7e578d94b228302c549e35ad5f9e
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-30 01:51:28 +0600

    final commit

--- index.js
+++ index.js
@@ -97,7 +97,13 @@ async function run() {
       const users = await usersCollection.find(query).toArray();
       res.send(users);
     });
+    app.get('/allphones', async (req, res) => {
+      const query = {};
+      const phones = await phoneCollection.find(query).toArray();
+      res.send(phones);
+    });
 
+    // Admin get response to client
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email };
@@ -105,12 +111,26 @@ async function run() {
       res.send({ isAdmin: user?.role === 'admin' });
     });
 
+    // Seller get response to client
+    app.get('/users/seller/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === 'seller' });
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+    app.post('/allphones', async (req, res) => {
+      const phone = req.body;
+      const result = await phoneCollection.insertOne(phone);
+      res.send(result);
+    });
 
+    // Making an Admin
     app.put('/users/admin/:id', verifyJWT, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const query = { email: decodedEmail };
